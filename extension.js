d536b533008374a0e61bb34f22061d3ac345bf4f
Ewan Drummond <45878916+edrumm@users.noreply.github.com>
2022-07-30 18:07:53 +0100

    setup some commands and add license

--- extension.js
+++ extension.js
@@ -1,33 +1,48 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+/*
+	Extension description
+*/
 const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vs-bookmark" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vs-bookmark.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vs-bookmark!');
+	const toggle = vscode.commands.registerCommand('vs-bookmark.toggle', () => {
+		vscode.window.showInformationMessage('Toggle');
 	});
 
-	context.subscriptions.push(disposable);
+	const jump = vscode.commands.registerCommand('vs-bookmark.jump', () => {
+		vscode.window.showInformationMessage('Jump');
+	});
+
+	const list = vscode.commands.registerCommand('vs-bookmark.list', () => {
+		vscode.window.showInformationMessage('List');
+	});
+
+	const listAll = vscode.commands.registerCommand('vs-bookmark.listAll', () => {
+		vscode.window.showInformationMessage('List all');
+	});
+
+	const clear = vscode.commands.registerCommand('vs-bookmark.clear', () => {
+		vscode.window.showInformationMessage('Clear');
+	});
+
+	const clearAll = vscode.commands.registerCommand('vs-bookmark.clearAll', () => {
+		vscode.window.showInformationMessage('Clear all');
+	});
+
+	// context.subscriptions.push(activate, toggle);
+	context.subscriptions.concat([
+		toggle,
+		jump,
+		list,
+		listAll,
+		clear,
+		clearAll
+	]);
 }
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
